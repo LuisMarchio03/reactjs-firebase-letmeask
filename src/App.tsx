4e10e410ae2f62871182b32eb4b3cef20107fdda
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2021-06-27 11:00:56 -0300

    Dark Mode / Light Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import {AuthContextProvider} from './contexts/AuthContext';
-import { ThemeContextProvider } from "./contexts/ThemeContext";
+
+import { useTheme } from "./hooks/useTheme";
 
 import { AdminRoom } from "./pages/AdminRoom";
 import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
 
+import './styles/theme.scss';
+
 function App() {
+  const { theme } = useTheme();
+  
   return (
-    <BrowserRouter>
-      <ThemeContextProvider>
-          <AuthContextProvider>
-              <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/rooms/new" component={NewRoom} />
-                <Route path="/rooms/:id" component={Room} />
-                <Route path="/admin/rooms/:id" component={AdminRoom} />
-              </Switch>
-          </AuthContextProvider>
-      </ThemeContextProvider>
-    </BrowserRouter>
+    <div id="theme" className="dark">
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
+          </Switch>
+        </AuthContextProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
